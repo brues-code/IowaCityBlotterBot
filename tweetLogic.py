517bf078f4f3a8d34b0492236f82729f759d9027
Skyler Hiscock <skylerhiscock@gmail.com>
2019-01-09 11:48:16 -0600

    Print with timestamp

--- tweetLogic.py
+++ tweetLogic.py
@@ -1,8 +1,11 @@
+import datetime
+import emoji
+from tweepy import TweepError
 from fetchBlotter import fetch
 from settings import settings
 from tweetBlot import tweet
 from tweetResult import TweetResult
-from tweepy import TweepError
+
 
 blotFetcher = fetch()
 settings = settings()
@@ -10,6 +13,21 @@ tweet = tweet()
 
 blockedTweets = ["created from mobile", "cfs"]
 eventBlock = ["event", "evnt", "ref amb"]
+
+def printWithStamp( inputStr ):
+    st = datetime.datetime.now().strftime('%H:%M:%S')
+    outputStr = "[%s]: %s" % (st, inputStr)
+    print(outputStr)
+
+
+def appendEmojis( inputStr ):
+    splitStr = inputStr.split()
+    emojiOutput = " "
+    for word in splitStr:
+        emojiStr = ":" + word.lower() + ":"
+        if (emojiStr in emoji.EMOJI_ALIAS_UNICODE or emojiStr in emoji.EMOJI_UNICODE) and len(word) > 2:
+            emojiOutput = emojiOutput + emojiStr
+    return emoji.emojize(inputStr + emojiOutput, use_aliases=True)
 
 class tweetLogic:
     def __init__(self):
@@ -19,7 +37,7 @@ class tweetLogic:
     def updateIds(self):
         if len(self.dispatchIds) == 0:
             newIds = blotFetcher.fetchDispatchIds()
-            print("Added %s new ids" % (len(newIds)))
+            printWithStamp("Added %s new ids" % (len(newIds)))
             self.dispatchIds = newIds
 
     def tweetStatus(self):
@@ -32,21 +50,22 @@ class tweetLogic:
             if len(dispatchMsg) > 2 and idToTweet > self.lastDispatchId and blockedTweetsLen == 0 and eventFilter == 0:
                 try:
                     dispatchUrl = "%s?dis=%s" % (settings.getRootUrl(), idToTweet)
+                    #emojizedStr = appendEmojis(dispatchMsg)
                     tweetMsg = "%s\n%s" % (dispatchMsg, dispatchUrl)
                     newTweet = tweet.sendStatus(tweetMsg)
                     newTweetUrl = "https://twitter.com/%s/status/%s" % (newTweet.user.screen_name, newTweet.id_str)
-                    print("%s : '%s'" % (newTweetUrl, tweetMsg))
+                    printWithStamp("%s : '%s'" % (newTweetUrl, tweetMsg))
                     result = TweetResult.SENT
                 except TweepError as e:
-                    print("Twitter error #%s: '%s'" % (idToTweet, str(e)))
+                    printWithStamp("Twitter error #%s: '%s'" % (idToTweet, str(e)))
                     result = TweetResult.TWITTER_ERROR
             else:
-                print("Didn't tweet #%s: '%s'" % (idToTweet, dispatchMsg))
+                printWithStamp("Didn't tweet #%s: '%s'" % (idToTweet, dispatchMsg))
                 result = TweetResult.IGNORED
             settings.saveDispatchId(idToTweet)
             self.lastDispatchId = idToTweet
         else:
-            print("Nothing to tweet...")
+            printWithStamp("Nothing to tweet...")
         return result
 
     def sendNext(self):
