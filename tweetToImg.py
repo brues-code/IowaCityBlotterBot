3b4f9db3c5f4ec5b93070edda24e61683a3048e1
brues-code <skylerhiscock@gmail.com>
2022-10-30 17:24:17 -0500

    updated crop for image

--- tweetToImg.py
+++ tweetToImg.py
@@ -1,15 +1,18 @@
 import os
 import imgkit
-from fetchBlotter import fetchSoup
+from fetchBlotter import fetch
 from settings import settings
+from tweetBlot import tweet
 
 settings = settings()
+fetcher = fetch()
+tweet = tweet()
 
 IMAGE_DIRECTORY = 'images/'
 CSS_FILE = 'blotstyle.css'
 IMAGE_OPTIONS = {
     'format': 'png',
-    'crop-w': '470',
+    'crop-w': '478',
     'quiet': ''
 }
 if os.name == 'posix':
@@ -29,4 +32,14 @@ class tweetToImg:
 
         fileName = '%s%s.%s' % (imgDirectory, dispatchId, IMAGE_OPTIONS['format'])
         imgkit.from_string(str(detailList), fileName,
-                           options=IMAGE_OPTIONS, css=CSS_FILE)
+                            options=IMAGE_OPTIONS, css=CSS_FILE)
+        return fileName
+
+# dispatchId = "22169064"
+# tweetSoup = fetcher.fetchDispatchDetails(dispatchId)
+# tweetToImg = tweetToImg()
+
+# fileName = tweetToImg.convertTweetToImage(dispatchId, tweetSoup)
+# tweet.sendStatus(fileName=fileName)
+
+#tweet.sendImageStatus(fileName)
